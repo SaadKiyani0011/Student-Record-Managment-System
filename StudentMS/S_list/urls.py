b92e53e2d346b270ad54b21c1969db4655a62a7e
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-14 15:35:57 +0500

    .

--- StudentMS/S_list/urls.py
+++ StudentMS/S_list/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (
+from S_list.views import (
     StudentListView,
     StudentCreateView,
     StudentUpdateView,
--- StudentMS/StudentMS/settings.py
+++ StudentMS/StudentMS/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'S_list',
     'S_add',
     'S_edit',
+    'base',
 ]
 
 MIDDLEWARE = [
